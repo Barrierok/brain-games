a5a40e7f37d94d236997e21bdb638b02e44122f5
Barrier <ilyazubtsov@gmail.com>
2019-08-20 13:43:17 +0300

    Adding game management

---
+++ src/games/index.js
@@ -0,0 +1,4 @@
+import * as calc from './calc';
+import * as even from './even';
+
+export default { calc, even };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { question as ask } from 'readline-sync';
-import * as calc from './games/calc';
-import * as even from './games/even';
+import variation from './games';
 
 export default (game = null) => {
   if (!game) {
     console.log('Welcome, enter the name of the game in your console');
     return;
   }
-  const variation = { calc, even };
 
   console.log(`Welcome to the Brain Games!\n${variation[game].greeting}`);
 
@@ -15,7 +13,6 @@ export default (game = null) => {
   console.log(`Hello, ${userName}!\n`);
 
   const expectedCorrect = 3;
-
   const iter = (correctAnswers) => {
     if (correctAnswers === expectedCorrect) {
       console.log(`Congratulations, ${userName}!`);
