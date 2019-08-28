6ac8d1db964e63415e64f47b99cb4d1f44dc7b29
Barrier <ilyazubtsov@gmail.com>
2019-08-28 22:54:38 +0300

    fix error

--- src/games/calc.js
+++ src/games/calc.js
@@ -21,7 +21,7 @@ const gameDescription = 'What is the result of the expression?';
 const getData = () => {
   const operand1 = random(0, 100);
   const operand2 = random(0, 100);
-  const operation = operators(random(0, operators.length));
+  const operation = operators[random(0, operators.length)];
 
   const result = {
     question: `${operand1} ${operation} ${operand2}`,
